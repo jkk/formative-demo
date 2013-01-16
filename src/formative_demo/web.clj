ad8ba9246483dab28bc5d0a692e8c80eef158423
Justin Kramer <jkkramer@gmail.com>
2013-01-16 12:06:51 -0500

    show table renderer

--- src/formative_demo/web.clj
+++ src/formative_demo/web.clj
@@ -46,7 +46,8 @@
    :fields [{:name :renderer
              :type :select
              :options ["bootstrap-horizontal"
-                       "bootstrap-stacked"]
+                       "bootstrap-stacked"
+                       "table"]
              :onchange "this.form.submit()"}]})
 
 (defn layout [& body]
@@ -55,7 +56,17 @@
      [:title "Formative Demo"]
      (page/include-css "//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.2.2/css/bootstrap.min.css")
      (page/include-css "//google-code-prettify.googlecode.com/svn/trunk/src/prettify.css")
-     [:style "body { margin: 2em; }"]]
+     [:style
+      "body { margin: 2em; }"
+      ".form-table { width: 100%; }"
+      ".form-table th { text-align: left; }"
+      ".form-table h3 { border-bottom: 1px solid #ddd; }"
+      ".form-table .label-cell { vertical-align: top; text-align: right; padding-right: 10px; padding-top: 10px; }"
+      ".form-table td { vertical-align: top; padding-top: 5px; }"
+      ".form-table .checkbox-row label { display: inline; margin-left: 5px; }"
+      ".form-table .checkbox-row .input-shell { margin-bottom: 10px; }"
+      ".form-table .submit-row th, .form-table .submit-row td { padding: 30px 0; }"
+      ".form-table .problem th, .form-table .problem td { color: #b94a48; background: #fee; }"]]
     [:body {:onload "prettyPrint()"}
      body]
     (page/include-js "//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js")
